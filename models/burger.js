2f35e341fe3434fab9b5e68ffad8812c655e0a8f
Nikita Sasunkevich <nikita@Nikitas-MacBook-Pro.local>
2018-02-24 00:19:20 -0700

    fixed bug with default value on Burger model

--- models/burger.js
+++ models/burger.js
@@ -11,7 +11,7 @@ module.exports = function(sequelize, DataTypes){
         },
         devoured: {
             type: DataTypes.BOOLEAN,
-            default: 0
+            defaultValue: false
         }
     });
 
