855c537c4303882617c4745234080bd16b9613b9
Nikita Sasunkevich <nikita@nikitas-mbp.lan>
2018-03-01 23:37:26 -0700

    removed id column as sequelize generates it itself

--- models/burger.js
+++ models/burger.js
@@ -1,10 +1,5 @@
 module.exports = function(sequelize, DataTypes){
     var Burger = sequelize.define("Burger", {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true
-        },
         burger_name: {
             type: DataTypes.STRING,
             allowNull: false
